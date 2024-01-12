c72795af1df54c5f349d1150bda9d62c368cbba5
CodingAbdullah <abdullahmd326@gmail.com>
2024-01-12 16:00:23 -0500

    Frontend Farmer prices page complete

--- frontend/src/Components/FarmerPricePage/FarmerPricePage.tsx
+++ frontend/src/Components/FarmerPricePage/FarmerPricePage.tsx
@@ -1,23 +1,108 @@
 import { FC } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { wolfGameFarmerFloorPrice } from '../../utilFunctions/wolfGameFarmerPrice';
+import { wolfGameFarmerAAJPrice, wolfGameFarmerAverageJoePrice, 
+            wolfGameFarmerBigDaddyPrice, wolfGameFarmerBigDieselPrice, 
+            wolfGameFarmerBigMamaPrice, wolfGameFarmerFloorPrice, 
+            wolfGameFarmerHundoPrice } from '../../utilFunctions/wolfGameFarmerPrice';
 import FloorPriceLabel from '../FloorPriceLabel/FloorPriceLabel';
 
-// Table representing floor prices for Average Joe, AAJ, Big Mama, Big Daddy, Diesel, Hundos..
+// Table representing floor prices for Farmers, Average Joe, AAJ, Big Mama, Big Daddy, Diesel, Hundos
 // Extreme floor price
 // Table detailing prices by trait
 const FarmerPricePage: FC = () => {
 
+    // Set up queries to run using React Query to used to fetch all farmer floor prices
     const farmerFloorPriceQuery = useQuery({
         queryKey: ['farmer floor price'],
         queryFn: wolfGameFarmerFloorPrice
     });
 
-    return (
-        <div className='farmer-price-page'>
-            <FloorPriceLabel heading={["Wolf Game Farmer Prices"]} price={[0]} />
-        </div>
-    )
+    const farmerAverageJoePriceQuery = useQuery({
+        queryKey: ['average joe price'],
+        queryFn: wolfGameFarmerAverageJoePrice
+    });
+
+    const farmerAboveAverageJanePriceQuery = useQuery({
+        queryKey: ['above average jane price'],
+        queryFn: wolfGameFarmerAAJPrice
+    });
+
+    const farmerDieselPriceQuery = useQuery({
+        queryKey: ['diesel price'],
+        queryFn: wolfGameFarmerBigDieselPrice
+    });
+
+    const farmerBigMamaPriceQuery = useQuery({
+        queryKey: ['big mama price'],
+        queryFn: wolfGameFarmerBigMamaPrice
+    });
+
+    const farmerBigDaddyPriceQuery = useQuery({
+        queryKey: ['big daddy price'],
+        queryFn: wolfGameFarmerBigDaddyPrice
+    });
+
+    const farmerHundoPriceQuery = useQuery({
+        queryKey: ['hundo price'],
+        queryFn: wolfGameFarmerHundoPrice
+    });
+
+    if (farmerFloorPriceQuery.isPending || farmerAboveAverageJanePriceQuery.isPending || 
+        farmerAverageJoePriceQuery.isPending || farmerBigDaddyPriceQuery.isPending || 
+        farmerBigMamaPriceQuery.isPending || farmerDieselPriceQuery.isPending || 
+        farmerHundoPriceQuery.isPending){
+            <div>Loading data..</div>
+    }
+    else if (farmerFloorPriceQuery.isSuccess || 
+            farmerAboveAverageJanePriceQuery.isSuccess || 
+            farmerAverageJoePriceQuery.isSuccess || 
+            farmerBigDaddyPriceQuery.isSuccess || 
+            farmerBigMamaPriceQuery.isSuccess || 
+            farmerDieselPriceQuery.isSuccess || 
+            farmerHundoPriceQuery.isSuccess) {
+                return (
+                    <div className='farmer-price-page'>
+                        <FloorPriceLabel heading={["Wolf Game Farmer Prices"]} price={[0]} />
+                        <table className="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Farmer Assets</th>
+                                    <th scope="col">Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Average Joe</th>
+                                    <td>{ farmerAverageJoePriceQuery.data.price }</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Above Average Jane</th>
+                                    <td>{ farmerAboveAverageJanePriceQuery.data.price }</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Diesel</th>
+                                    <td>{ farmerDieselPriceQuery.data.price }</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Hundo</th>
+                                    <td>{ farmerHundoPriceQuery.data.price }</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Big Mama</th>
+                                    <td>{ farmerBigMamaPriceQuery.data.price }</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Big Daddy</th>
+                                    <td>{ farmerBigDaddyPriceQuery.data.price }</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                )    
+    }
+    else {
+        return <div>Error loading data..</div>
+    }
 }
 
 export default FarmerPricePage;
--- frontend/src/utilFunctions/wolfGameFarmerPrice.ts
+++ frontend/src/utilFunctions/wolfGameFarmerPrice.ts
@@ -1,29 +1,85 @@
 import axios from 'axios';
 
-export const wolfGameFarmerFloorPrice = () => {
-    // Code to go here..
+// Fetch Farmer floor price
+export const wolfGameFarmerFloorPrice = async () => {
+    const response = await axios.get('https://localhost:5000/wolf-game-farmer-floor-price');
+
+    if (response.status === 200){
+        return response.data;
+    }
+    else {
+        throw new Error("Could not fetch Wolf Game Farmer data");
+    }
 }
 
-export const wolfGameFarmerHundoPrice = () => {
-    // Code to go here..
+// Fetch Hundo floor price
+export const wolfGameFarmerHundoPrice = async () => {
+    const response = await axios.get('https://localhost:5000/wolf-game-farmer-hundo-price');
+
+    if (response.status === 200){
+        return response.data;
+    }
+    else {
+        throw new Error("Could not fetch Wolf Game Farmer data");
+    }
 }
 
-export const wolfGameFarmerAAJPrice = () => {
-    // Code to go here..
+// Fetch Above Average Jane floor price
+export const wolfGameFarmerAAJPrice = async () => {
+    const response = await axios.get('https://localhost:5000/wolf-game-farmer-average-joe-price');
+
+    if (response.status === 200){
+        return response.data;
+    }
+    else {
+        throw new Error("Could not fetch Wolf Game Farmer data");
+    }
 }
 
-export const wolfGameFarmerBigMamaPrice = () => {
-    // Code to go here..
+// Fetch Big Mama floor price
+export const wolfGameFarmerBigMamaPrice = async () => {
+    const response = await axios.get('https://localhost:5000/wolf-game-farmer-big-mama-price');
+
+    if (response.status === 200){
+        return response.data;
+    }
+    else {
+        throw new Error("Could not fetch Wolf Game Farmer data");
+    }
 }
 
-export const wolfGameFarmerBigDaddyPrice = () => {
-    // Code to go here..
+// Fetch Big Daddy price
+export const wolfGameFarmerBigDaddyPrice = async () => {
+    const response = await axios.get('https://localhost:5000/wolf-game-farmer-big-daddy-price');
+
+    if (response.status === 200){
+        return response.data;
+    }
+    else {
+        throw new Error("Could not fetch Wolf Game Farmer data");
+    }
 }
 
-export const wolfGameFarmerBigDieselPrice = () => {
-    // Code to go here..
+// Fetch Big Diesel floor price
+export const wolfGameFarmerBigDieselPrice = async () => {
+    const response = await axios.get('https://localhost:5000/wolf-game-farmer-diesel-price');
+
+    if (response.status === 200){
+        return response.data;
+    }
+    else {
+        throw new Error("Could not fetch Wolf Game Farmer data");
+    }
 }
 
-export const wolfGameFarmerAverageJoePrice = () => {
-    // Code to go here..
+// Fetch Average Joe floor price
+export const wolfGameFarmerAverageJoePrice = async () => {
+    const response = await axios.get('https://localhost:5000/wolf-game-farmer-hundo-price');
+
+    if (response.status === 200){
+        return response.data;
+    }
+    else {
+        throw new Error("Could not fetch Wolf Game Farmer data");
+    }
 }
