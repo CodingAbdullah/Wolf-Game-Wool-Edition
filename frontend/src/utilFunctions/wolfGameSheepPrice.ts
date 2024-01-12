a909df11646058b3a8cdf55d3381a446845d2a50
CodingAbdullah <abdullahmd326@gmail.com>
2024-01-12 18:46:27 -0500

    Generation 2 util functions added

--- frontend/src/utilFunctions/wolfGameSheepPrice.ts
+++ frontend/src/utilFunctions/wolfGameSheepPrice.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const wolfGameSheepFloorPrice = () => {
     // Code to go here..
 }
+
+export const wolfGameSheepGen2FloorPrice = () => {
+    // Code to go here..
+}
--- frontend/src/utilFunctions/wolfGameWolfPrice.ts
+++ frontend/src/utilFunctions/wolfGameWolfPrice.ts
@@ -4,7 +4,15 @@ export const wolfGameWolfFloorPrice = () => {
     // Code to go here..
 }
 
+export const wolfGameWolfGen2FloorPrice = () => {
+    // Code to go here..
+}
+
 export const wolfGameWolfA5Price = () => {
+    // Code to go here..
+}
+
+export const wolfGameWolfA5Gen2Price = () => {
     // Code to go here..
 }
 
@@ -12,8 +20,16 @@ export const wolfGameWolfA6Price = () => {
     // Code to go here..
 }
 
+export const wolfGameWolfA6Gen2Price = () => {
+    // Code to go here..
+}
+
 export const wolfGameWolfA7Price = () => {
     // Code to go here..  
+}
+
+export const wolfGameWolfA7Gen2Price = () => {
+    // Code to go here..
 }
 
 export const wolfGameWolfA8Price = () => {
