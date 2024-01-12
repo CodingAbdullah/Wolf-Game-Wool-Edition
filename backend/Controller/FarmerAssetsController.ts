18b9f712d54f77ea2957d84e8bc33a43ab97ad04
CodingAbdullah <abdullahmd326@gmail.com>
2024-01-12 16:22:06 -0500

    Farmer Assets Controller backend implementation complete

--- backend/Controller/FarmerAssetsController.ts
+++ backend/Controller/FarmerAssetsController.ts
@@ -1,31 +1,138 @@
 import { Request, Response } from 'express';
+import axios from 'axios';
+import { SIMPLEHASH_URL as API_URL } from '../util/simpleHashAPIURLEndpoint';
+import * as dotenv from 'dotenv';
+
+// Set configuration to path where the .env file resides in development environment
+dotenv.config({ path: '../.env '});
+
+// Set options for API request
+const options = {
+    method: 'GET',
+    headers: {
+        'accept' : 'application/json',
+        'content-type': 'application/json',
+        'X-API-KEY' : process.env.SIMPLE_HASH_API_KEY
+    }
+}
 
 const getFarmerFloorPrice = (req: Request, res: Response) => {
-    // Code to go here..
+
+    // Fetch data related to farmer floor price
+    axios.get(API_URL + '/traits/collection/' + process.env.WOLF_GAME_FARMER_COLLECTION_ID + 
+             '/floor?trait_type=Type&value=Average%20Joe', options)
+    .then(response => {
+        res.status(200).json({
+            price: response.data.floor_price.value
+        });
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: "Could not fetch farmer floor price data " + err
+        });
+    });
 }
 
 const getFarmerHundoPrice = (req: Request, res: Response) => {
-    // Code to go here..
+
+    // Fetch data related to farmer floor price
+    axios.get(API_URL + '/traits/collection/' + process.env.WOLF_GAME_FARMER_COLLECTION_ID + 
+             '/floor?trait_type=Type&value=Hundred%20Years', options)
+    .then(response => {
+        res.status(200).json({
+            price: response.data.floor_price.value
+        });
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: "Could not fetch farmer floor price data " + err
+        });
+    });    
 }
 
 const getFarmerAverageJoePrice = (req: Request, res: Response) => {
-    // Code to go here..
+    
+    // Fetch data related to farmer floor price
+    axios.get(API_URL + '/traits/collection/' + process.env.WOLF_GAME_FARMER_COLLECTION_ID + 
+             '/floor?trait_type=Type&value=Average%20Joe', options)
+    .then(response => {
+        res.status(200).json({
+            price: response.data.floor_price.value
+        });
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: "Could not fetch farmer floor price data " + err
+        });
+    });
 }
 
 const getFarmerAboveAverageJanePrice = (req: Request, res: Response) => {
-    // Code to go here..
+
+    // Fetch data related to farmer floor price
+    axios.get(API_URL + '/traits/collection/' + process.env.WOLF_GAME_FARMER_COLLECTION_ID + 
+             '/floor?trait_type=Type&value=Above%20Average%20Jane', options)
+    .then(response => {
+        res.status(200).json({
+            price: response.data.floor_price.value
+        });
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: "Could not fetch farmer floor price data " + err
+        });
+    });
 }
 
 const getFarmerDieselPrice = (req: Request, res: Response) => {
-    // Code to go here..
+
+    // Fetch data related to farmer floor price
+    axios.get(API_URL + '/traits/collection/' + process.env.WOLF_GAME_FARMER_COLLECTION_ID + 
+             '/floor?trait_type=Type&value=Diesel', options)
+    .then(response => {
+        res.status(200).json({
+            price: response.data.floor_price.value
+        });
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: "Could not fetch farmer floor price data " + err
+        });
+    });
 }
 
 const getFarmerBigMamaPrice = (req: Request, res: Response) => {
-    // Code to go here..
+
+    // Fetch data related to farmer floor price
+    axios.get(API_URL + '/traits/collection/' + process.env.WOLF_GAME_FARMER_COLLECTION_ID + 
+             '/floor?trait_type=Type&value=Big%20Mama', options)
+    .then(response => {
+        res.status(200).json({
+            price: response.data.floor_price.value
+        });
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: "Could not fetch farmer floor price data " + err
+        });
+    });
 }
 
 const getFarmerBigDaddyPrice = (req: Request, res: Response) => {
-    // Code to go here..
+    
+    // Fetch data related to farmer floor price
+    axios.get(API_URL + '/traits/collection/' + process.env.WOLF_GAME_FARMER_COLLECTION_ID + 
+             '/floor?trait_type=Type&value=Big%20Daddy', options)
+    .then(response => {
+        res.status(200).json({
+            price: response.data.floor_price.value
+        });
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: "Could not fetch farmer floor price data " + err
+        });
+    });
 }
 
 // Mapping functions to be used
---
+++ backend/util/simpleHashAPIURLEndpoint.ts
@@ -0,0 +1,2 @@
+// Exporting API URL for modularity
+export const SIMPLEHASH_URL='https://api.simplehash.com/api/v0/nfts';
