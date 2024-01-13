2aee620437eb3c04ef6904b8f1f197951befc06f
CodingAbdullah <abdullahmd326@gmail.com>
2024-01-12 19:45:49 -0500

    Adding middleware and API routes to Express server

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,7 @@
 import express, { Express } from 'express';
+import FarmerAssetRoute from './Routes/FarmerAssetsRoute';
+import GameAssetRoute from './Routes/GameAssetsRoute';
+import LandAssetRoute from './Routes/LandAssetsRoute';
 import cors from 'cors';
 
 // Spin up Node server
@@ -7,6 +10,11 @@ const app: Express = express();
 // Listening on Port 5000
 app.listen(5000, () => console.log("Listening to PORT 5000"));
 
+// Add in middleware as well as API routes
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/", FarmerAssetRoute);
+app.use("/", GameAssetRoute);
+app.use("/", LandAssetRoute);
